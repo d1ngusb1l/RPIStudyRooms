ae9dc961f48db211eb196cc72b3f37e32f6a24ea
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2024-07-10 21:37:17 -0400

    Add Prisma

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,10 @@
 import {type Express} from "express";
+import prisma from "../db.js";
 
 /* GET users listing. */
 export default function setUsersRoutes(app: Express) {
-  app.get('/api/users', function (req, res, next) {
-    res.send(JSON.stringify({ "hello": "world" }))
+  app.get('/api/users', async function (req, res, next) {
+    const users = await prisma.user.findMany();
+    res.json(users);
   });
 }
