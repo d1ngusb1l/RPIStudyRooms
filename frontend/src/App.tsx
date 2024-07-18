364444f1e9b8ad35eb354ac307f28a2ef2f63c84
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2024-07-17 23:28:55 -0400

    Fix render issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 //import { useEffect, useState } from 'react';
-import React, { useCallback, useState } from 'react';
+import React, { createContext, useCallback, useContext, useReducer, useState } from 'react';
 import './App.css'
 import Collapsible from "./Collapsible";
 //import { Type, type Static } from '@sinclair/typebox'
@@ -17,89 +17,90 @@ type Room = Static<typeof RoomDef>
 const RoomArrayDef = Type.Array(RoomDef)
 */
 
+interface Room {
+  number: string;
+  status: "empty" | "full";
+  lastReported: Date;
+}
 
-const rooms = [
-  {number: "323-A", status: "empty", lastReported: new Date()},
-  {number: "332-A", status: "empty", lastReported: new Date()},
-  {number: "332-B", status: "empty", lastReported: new Date()},
-  {number: "332-C", status: "empty", lastReported: new Date()},
-  {number: "332-D", status: "empty", lastReported: new Date()},
-  {number: "332-E", status: "empty", lastReported: new Date()},
-  {number: "337-A", status: "empty", lastReported: new Date()},
-  {number: "337-B", status: "empty", lastReported: new Date()},
-  {number: "337-C", status: "empty", lastReported: new Date()},
-  {number: "337-D", status: "empty", lastReported: new Date()},
-  {number: "337-E", status: "empty", lastReported: new Date()},
-  {number: "332-A", status: "empty", lastReported: new Date()},
-  {number: "342-B", status: "empty", lastReported: new Date()},
-  {number: "342-C", status: "empty", lastReported: new Date()},
-  {number: "352-A", status: "empty", lastReported: new Date()},
-  {number: "352-B", status: "empty", lastReported: new Date()},
-  {number: "352-C", status: "empty", lastReported: new Date()},
-  {number: "352-D", status: "empty", lastReported: new Date()},
-  {number: "352-E", status: "empty", lastReported: new Date()},
-  {number: "423-A", status: "empty", lastReported: new Date()},
-  {number: "423-B", status: "empty", lastReported: new Date()},
-  {number: "423-C", status: "empty", lastReported: new Date()},
-  {number: "423-D", status: "empty", lastReported: new Date()},
-  {number: "423-E", status: "empty", lastReported: new Date()},
-  {number: "424-A", status: "empty", lastReported: new Date()},
-  {number: "424-B", status: "empty", lastReported: new Date()},
-  {number: "433-A", status: "empty", lastReported: new Date()},
-  {number: "435-D", status: "empty", lastReported: new Date()},
-  {number: "437-A", status: "empty", lastReported: new Date()},
-  {number: "437-B", status: "empty", lastReported: new Date()},
-  {number: "437-C", status: "empty", lastReported: new Date()},
-  {number: "423-A", status: "empty", lastReported: new Date()},
-  {number: "423-B", status: "empty", lastReported: new Date()},
-  {number: "423-C", status: "empty", lastReported: new Date()},
-  {number: "442-A", status: "empty", lastReported: new Date()},
-  {number: "442-B", status: "empty", lastReported: new Date()},
-  {number: "442-C", status: "empty", lastReported: new Date()},
-  {number: "442-D", status: "empty", lastReported: new Date()},
-  {number: "442-E", status: "empty", lastReported: new Date()},
-  {number: "453-A", status: "empty", lastReported: new Date()},
-  {number: "453-B", status: "empty", lastReported: new Date()},
-  {number: "453-C", status: "empty", lastReported: new Date()},
-  {number: "453-D", status: "empty", lastReported: new Date()},
-  {number: "453-E", status: "empty", lastReported: new Date()},
-];
+type RoomUpdateAction = { type: "single", number: string, newData: Partial<Omit<Room, "number" | "lastReported">> } | { type: "all", data: Room[] };
 
-function EmptyButton({rNum} : {rNum:string}) {
 
-  function handleClick() {
-    let room = rooms.find((room) => room.number === rNum);
-    if(!room) throw new Error("invalid room number");
-    room.status = "empty"; 
-    room.lastReported = new Date();
-  }
+const initialRooms = [
+  { number: "323-A", status: "empty", lastReported: new Date() },
+  { number: "332-A", status: "empty", lastReported: new Date() },
+  { number: "332-B", status: "empty", lastReported: new Date() },
+  { number: "332-C", status: "empty", lastReported: new Date() },
+  { number: "332-D", status: "empty", lastReported: new Date() },
+  { number: "332-E", status: "empty", lastReported: new Date() },
+  { number: "337-A", status: "empty", lastReported: new Date() },
+  { number: "337-B", status: "empty", lastReported: new Date() },
+  { number: "337-C", status: "empty", lastReported: new Date() },
+  { number: "337-D", status: "empty", lastReported: new Date() },
+  { number: "337-E", status: "empty", lastReported: new Date() },
+  { number: "332-A", status: "empty", lastReported: new Date() },
+  { number: "342-B", status: "empty", lastReported: new Date() },
+  { number: "342-C", status: "empty", lastReported: new Date() },
+  { number: "352-A", status: "empty", lastReported: new Date() },
+  { number: "352-B", status: "empty", lastReported: new Date() },
+  { number: "352-C", status: "empty", lastReported: new Date() },
+  { number: "352-D", status: "empty", lastReported: new Date() },
+  { number: "352-E", status: "empty", lastReported: new Date() },
+  { number: "423-A", status: "empty", lastReported: new Date() },
+  { number: "423-B", status: "empty", lastReported: new Date() },
+  { number: "423-C", status: "empty", lastReported: new Date() },
+  { number: "423-D", status: "empty", lastReported: new Date() },
+  { number: "423-E", status: "empty", lastReported: new Date() },
+  { number: "424-A", status: "empty", lastReported: new Date() },
+  { number: "424-B", status: "empty", lastReported: new Date() },
+  { number: "433-A", status: "empty", lastReported: new Date() },
+  { number: "435-D", status: "empty", lastReported: new Date() },
+  { number: "437-A", status: "empty", lastReported: new Date() },
+  { number: "437-B", status: "empty", lastReported: new Date() },
+  { number: "437-C", status: "empty", lastReported: new Date() },
+  { number: "423-A", status: "empty", lastReported: new Date() },
+  { number: "423-B", status: "empty", lastReported: new Date() },
+  { number: "423-C", status: "empty", lastReported: new Date() },
+  { number: "442-A", status: "empty", lastReported: new Date() },
+  { number: "442-B", status: "empty", lastReported: new Date() },
+  { number: "442-C", status: "empty", lastReported: new Date() },
+  { number: "442-D", status: "empty", lastReported: new Date() },
+  { number: "442-E", status: "empty", lastReported: new Date() },
+  { number: "453-A", status: "empty", lastReported: new Date() },
+  { number: "453-B", status: "empty", lastReported: new Date() },
+  { number: "453-C", status: "empty", lastReported: new Date() },
+  { number: "453-D", status: "empty", lastReported: new Date() },
+  { number: "453-E", status: "empty", lastReported: new Date() },
+] as const;
+
+const RoomContext = createContext<{ rooms: Room[], update: (action: RoomUpdateAction) => unknown }>({ rooms: [], update: () => undefined });
+
+function EmptyButton({ rNum }: { rNum: string }) {
+
+  const { update } = useContext(RoomContext);
 
   return (
-    <button onClick={handleClick}>
+    <button onClick={() => update({ type: "single", number: rNum, newData: { status: "empty" } })}>
       Report as Empty
     </button>
   );
 }
 
 
-function FullButton({rNum} : {rNum:string}) {
+function FullButton({ rNum }: { rNum: string }) {
 
-  function handleClick() {
-    let room = rooms.find((room) => room.number === rNum);
-    if(!room) throw new Error("invalid room number");
-    room.status = "empty"; 
-    room.lastReported = new Date();
-    
-  }
+  const { update } = useContext(RoomContext);
 
   return (
-    <button onClick={handleClick}>
+    <button onClick={() => update({ type: "single", number: rNum, newData: { status: "full" } })}>
       Report as Full
     </button>
   );
 }
 
 function ListRooms() {
+
+  const { rooms } = useContext(RoomContext);
 
   /*
   const [rooms, setRooms] = useState<Room[] | null>(null);
@@ -135,15 +136,15 @@ function ListRooms() {
 
   const listRooms = rooms.map(room =>
     <li key={room.number}>
-    <Collapsible title= {room.number}>
-    <p>
-        Reported as: {' ' + room.status + ' '}
-        at: {room.lastReported.toLocaleTimeString()}
-    </p>
-    <FullButton rNum={room.number}/>
-    <EmptyButton rNum={room.number}/>
-  </Collapsible>
-  </li>
+      <Collapsible title={room.number}>
+        <p>
+          Reported as: {' ' + room.status + ' '}
+          at: {room.lastReported.toLocaleTimeString()}
+        </p>
+        <FullButton rNum={room.number} />
+        <EmptyButton rNum={room.number} />
+      </Collapsible>
+    </li>
   );
   return <ul style={{ listStyle: 'none' }}>{listRooms}</ul>;
 }
@@ -159,11 +160,27 @@ function ScrollableList() {
 
 export default function MyApp() {
   //const [val, updateState] = useState(0);
+  // const [rooms, updateRooms] = useState<Room[]>([...initialRooms)
+  const [rooms, updateRooms] = useReducer((state: Room[], action: RoomUpdateAction) => {
+    switch (action.type) {
+      case "single": {
+        const roomIndex = state.findIndex((room) => room.number === action.number);
+        if (roomIndex === -1) throw new Error("invalid room number");
+        const newRoom: Room = { ...state[roomIndex], ...action.newData, lastReported: new Date() };
+        const copy = [...state];
+        copy[roomIndex] = newRoom;
+        return copy;
+      }
+      case "all": {
+        return action.data;
+      }
+    }
+  }, [...initialRooms]);
   return (
-    <div>
+    <RoomContext.Provider value={{ rooms, update: updateRooms }}>
       <h1>List of Rooms</h1>
       <ScrollableList />
-    </div>
+    </RoomContext.Provider>
   );
 }
 
