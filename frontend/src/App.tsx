d4eae802dd2596405edb40a4f2247fb4829c3327
hernae6 <148648790+hernae6@users.noreply.github.com>
2024-07-21 18:13:41 -0400

    added dropdown function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,6 +32,33 @@ function ScrollableList() {
   );
 }
 
+/* This is where the dropdown menu is handled! Edit the buttons in here to add the functions */
+function FloorDropdown(){
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Floors
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="dropdown-options">
+          <MenuItem>
+            <button>4</button>
+          </MenuItem>
+          <MenuItem>
+            <button>3</button>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+  )
+}
+
 export default function MyApp() {
   const [floors, setFloors] = useState<Floors | null>(null);
   const [currentFloor, setCurrentFloor] = useState("3");
